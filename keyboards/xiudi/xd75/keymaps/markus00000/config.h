1eb9b386ad0a62195a2b16a6f2f60083a6695f1a
Markus Weimar <mail@markusweimar.de>
2026-08-20 23:51:50 +0200

    Update ["xiudi/xd75", "markus00000"].

--- keyboards/xiudi/xd75/keymaps/markus00000/config.h
+++ keyboards/xiudi/xd75/keymaps/markus00000/config.h
@@ -32,10 +32,10 @@
 #define MK_C_OFFSET_2       1
 #define MK_C_INTERVAL_2     16
 #define MK_W_OFFSET_UNMOD   1
-#define MK_W_INTERVAL_UNMOD 40
+#define MK_W_INTERVAL_UNMOD 80
 #define MK_W_OFFSET_0       1
-#define MK_W_INTERVAL_0     360
+#define MK_W_INTERVAL_0     160
 #define MK_W_OFFSET_1       1
-#define MK_W_INTERVAL_1     120
+#define MK_W_INTERVAL_1     240
 #define MK_W_OFFSET_2       1
-#define MK_W_INTERVAL_2     20
+#define MK_W_INTERVAL_2     10
